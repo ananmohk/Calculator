760482e8318bdda0ac6c0ab26d8d85cf1c2c6427
Anan <mokzygamers@gmail.com>
2018-01-31 15:12:48 +0700

    calculatorAppTest

---
+++ app/src/main/java/com/example/poly/testapplication/Calculator.java
@@ -0,0 +1,21 @@
+package com.example.poly.testapplication;
+
+public class Calculator {
+    int plus(int x, int y) {
+        return x+y;
+    }
+    int minus(int x, int y) {
+        return x-y;
+    }
+    int multiply(int x, int y) {
+        return x*y;
+    }
+    int divide(int x, int y) {
+        if(y==0) {
+            return 0;
+        }
+        else {
+            return x / y;
+        }
+    }
+}
